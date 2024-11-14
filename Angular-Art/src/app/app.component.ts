6c6ceda4d32518f18c1fa5bd6a4a2375062b533c
Nate Aufdenberg <nlaufdenberg@gmail.com>
2024-11-14 12:27:40 -0600

    Started Component

--- Angular-Art/src/app/app.component.ts
+++ Angular-Art/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ArtComponent } from './art/art.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArtComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular-Art';
+  title = 'Art-Project';
 }
